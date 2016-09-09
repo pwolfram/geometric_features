3ae19524d021fc9cbe6ed550111b39095f89271f
Phillip J. Wolfram <phillipwolfram@gmail.com>
2016-09-09 17:07:39 -0600

    Uses new output from feature scripts to simplfy
    Also fixes minor formating issues

--- driver_scripts/setup_MOC_basins.py
+++ driver_scripts/setup_MOC_basins.py
@@ -1,71 +1,64 @@
 #!/usr/bin/env python
 """
-This script creates basins for computing the meridional overturning 
+This script creates basins for computing the meridional overturning
 circulation (MOC).  No arguments are required.  The optional --plot
 flag can be used to produce plots of each MOC basin.
+
+Authors: Xylar Asay-Davis, Phillip J. Wolfram
+Last Modified: 09/09/2016
 """
 import os
-import os.path
 import subprocess
-import shutil
 from optparse import OptionParser
+
+def spcall(args): #{{{
+    return subprocess.check_call(args, env=os.environ.copy()) #}}}
 
 parser = OptionParser()
 parser.add_option("--plot", action="store_true", dest="plot")
 
 options, args = parser.parse_args()
 
-defaultFileName = 'features.geojson'
 
+subBasins = {'Atlantic': ['Atlantic', 'Mediterranean'],
+             'IndoPacific': ['Pacific', 'Indian'],
+             'Pacific': ['Pacific'],
+             'Indian': ['Indian']}
 
-subBasins = {}
-subBasins['Atlantic'] = ['Atlantic', 'Mediterranean']
-subBasins['IndoPacific'] = ['Pacific', 'Indian']
-subBasins['Pacific'] = ['Pacific']
-subBasins['Indian'] = ['Indian']
-
-MOCMaskFileNames = {}
-MOCMaskFileNames['Atlantic'] = 'ocean/region/MOC_mask_30S/region.geojson'
-MOCMaskFileNames['IndoPacific'] = 'ocean/region/MOC_mask_30S/region.geojson'
-MOCMaskFileNames['Pacific'] = 'ocean/region/MOC_mask_6S/region.geojson'
-MOCMaskFileNames['Indian'] = 'ocean/region/MOC_mask_6S/region.geojson'
-
+MOCMaskFileNames = {'Atlantic': 'ocean/region/MOC_mask_30S/region.geojson',
+                    'IndoPacific': 'ocean/region/MOC_mask_30S/region.geojson',
+                    'Pacific': 'ocean/region/MOC_mask_6S/region.geojson',
+                    'Indian': 'ocean/region/MOC_mask_6S/region.geojson'}
 
 for basinName in subBasins:
 
-    if os.path.exists(defaultFileName):
-        os.remove(defaultFileName)
+    # output file names
+    basinFileName =  '%s_Basin.geojson'%basinName
+    imageName =  '%s_MOC.png'%basinName
+    MOCName =  '%s_MOC.geojson'%basinName
+
+    # remove old files (to ensure there isn't double-appending via merge_features
+    for afile in [basinFileName, imageName, MOCName]:
+        if os.path.exists(afile):
+            os.remove(afile)
 
     print " * merging features to make %s Basin"%basinName
-    MOCName =  '%s_MOC.geojson'%basinName
-    imageName =  '%s_MOC.png'%basinName
-    basinFileName =  '%s_Basin.geojson'%basinName
+
     for oceanName in subBasins[basinName]:
-        tag = '%s_Basin'%oceanName
-    
-        args = ['./merge_features.py', '-d', 'ocean', '-t', tag]
-        subprocess.check_call(args, env=os.environ.copy())
-    
-    shutil.move(defaultFileName,basinFileName)
-    
+        spcall(['./merge_features.py', '-d', 'ocean', '-t', '%s_Basin'%oceanName,
+                '-o', basinFileName])
+
     #merge the the features into a single file
     print " * combining features into single feature named %s_MOC"%basinName
-    args = ['./combine_features.py', '-f', basinFileName, '-n', '%s_MOC'%basinName]
-    #print ' '.join(args)
-    subprocess.check_call(args, env=os.environ.copy())
-    
-    shutil.move(defaultFileName,basinFileName)
-    
+    spcall(['./combine_features.py', '-f', basinFileName, '-n', '%s_MOC'%basinName,
+            '-o', basinFileName])
+
     print " * masking out features south of MOC region"
-    args = ['./difference_features.py', '-f', basinFileName, '-m', MOCMaskFileNames[basinName]]
-    subprocess.check_call(args, env=os.environ.copy())
-    
-    shutil.move(defaultFileName,MOCName)
-    
+    spcall(['./difference_features.py', '-f', basinFileName,
+            '-m', MOCMaskFileNames[basinName], '-o', MOCName])
+
     if options.plot:
-        args = ['./plot_features.py', '-f', MOCName, '-o', imageName, '-m', 'cyl']
-        subprocess.check_call(args, env=os.environ.copy())
-    
-    
-if os.path.exists(defaultFileName):
-    os.remove(defaultFileName)
+        spcall(['./plot_features.py', '-f', MOCName, '-o', imageName, '-m', 'cyl'])
+
+
+# vim: foldmethod=marker ai ts=4 sts=4 et sw=4 ft=python
